91642f495f3464e9959d03427e3d5ad029981166
Trần Văn Linh <133183567+LINHTRAN9x@users.noreply.github.com>
2023-08-29 12:08:43 +0700

    Add files via upload

--- AS6.sql
+++ AS6.sql
@@ -2,10 +2,11 @@ CREATE database AS6
 USE AS6
 
 select * from books 
+delete books 
 CREATE TABLE books(
     bookID varchar(255) PRIMARY KEY,
     bookName NVARCHAR(255),
-    tacgia varchar(255),
+    tacgiaID int,
     tomtat NVARCHAR(max),
     NamXuatBan int,
     LanXuatBan int,
@@ -14,18 +15,26 @@ CREATE TABLE books(
     SoLuong int,
     MaLoaiSach int,
     FOREIGN KEY (MaNXB) REFERENCES NhaXuatBan(MaNXB),
-    FOREIGN KEY (MaLoaiSach) REFERENCES LoaiSach(MaLoaiSach)
+    FOREIGN KEY (MaLoaiSach) REFERENCES LoaiSach(MaLoaiSach),
+    FOREIGN KEY (tacgiaID) REFERENCES tacgia(MaTacGia)
 )
-INSERT INTO books(bookID,bookName,tacgia,tomtat,NamXuatBan,LanXuatBan,MaNXB,GiaBan,SoLuong,MaLoaiSach) VALUES
-    ('B001',N'Trí tuệ đo Thái','Eran Katz',N'Bạn có muốn biết...',2010,1,10,79000,100,100)
+INSERT INTO books(bookID,bookName,tacgiaID,tomtat,NamXuatBan,LanXuatBan,MaNXB,GiaBan,SoLuong,MaLoaiSach) VALUES
+    ('B001',N'Trí tuệ do Thái',5,N'Bạn có muốn biết...',2010,1,10,79000,100,100),
+    ('B002',N'Thiên Đàng',5,N'Bạn có muốn biết...',2011,1,10,99000,100,200)
 
+CREATE TABLE tacgia(
+    MaTacGia int primary key,
+    TenTacGia nvarchar(255) not null
+)
+INSERT INTO tacgia(MaTacGia,TenTacGia) VALUES (5,'Eran Katz')
 
 CREATE TABLE LoaiSach (
     MaLoaiSach int primary key,
     TenLoaiSach nvarchar(255) not null
 );
 INSERT into LoaiSach(MaLoaiSach,TenLoaiSach) VALUES
-    (100,N'Khoa học xã hội')
+    (100,N'Khoa học xã hội'),
+    (200,N'Tiểu thuyết')
 
 CREATE TABLE NhaXuatBan (
     MaNXB int primary key,
